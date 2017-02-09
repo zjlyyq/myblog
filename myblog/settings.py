eca53f45f19a6b441da6726b5c7f930eb6f0f66e
zjlyyq <jialuzhang0805@gmail.com>
2017-02-09 11:26:24 +0800

    添加了一个操作数据库的app:students

--- myblog/settings.py
+++ myblog/settings.py
@@ -72,7 +72,6 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
