3e0cf43f5d47ef46a935a9d98fcca1dae27a99ef
zjl_in_home <2868989685@qq.com>
2017-02-12 16:39:24 +0800

    已经可以做简单的数据查询了

--- books/views.py
+++ books/views.py
@@ -4,14 +4,14 @@ from .models import Book
 # Create your views here.
 
 def search(request):
-    query = request.GET.get('q')
+    query = request.GET.get('q','')
     if query:
         qset = (
-            Q(title_icontains=query)|
-            Q(authors_first_name=query)|
-            Q(authors_last_name =query)
+            Q(title__icontains=query)|
+            Q(authors__first_name__icontains=query)|
+            Q(authors__last_name__icontains=query)                 #中间竟然是两个下划线
         )
         results = Book.objects.filter(qset).distinct()
     else:
         results = []
-    return render("search.html",{"results":results,"query":query})
+    return render(request,"search.html",{"results":results,"query":query})
